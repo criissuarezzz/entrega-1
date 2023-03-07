e8b5f66064730b41ce6f99db911413385c07cd13
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-07 18:15:43 +0100

    commit

--- EJERCICIOS/ejercicio2.py
+++ EJERCICIOS/ejercicio2.py
@@ -4,16 +4,14 @@ import sys
 numero_magico=12345679
 
 def numeromagico():
-    numeroint=input("Introduzca un numero del 0 al 9: ")
-    try:
-        numeroint=int(numeroint)
-    except:
-        print("valor no aceptado", file=sys.stderr)
+    numeroint=int(input("Introduzca un numero del 0 al 9: "))
     if 0<=numeroint<=9:
-        return numeroint
+
+        print(numeroint) 
     else:
         print("valor no aceptado", file=sys.stderr)
-        numero.numeromagico()
+        numeromagico()
+
     print("Has introducido el numero: ", numeroint)
     print("El numero magico es: ", numero_magico)
     print("Al multiplicar el numero introducido por 9, el resultado es: ", numeroint*9)
